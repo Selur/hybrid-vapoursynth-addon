c34946e190692fe43956c406f63ff345c8e58996
Georg Pelz <hybrid@selur.de>
2021-06-06 17:12:02 +0200

    added DPID: https://github.com/WolframRhodium/VapourSynth-dpid/ removed mvtoolssf: not needed anymore

---
+++ build-plugins/plugin-dpid.sh
@@ -0,0 +1,5 @@
+ghdl WolframRhodium/VapourSynth-dpid
+cd Source
+meson build
+ninja -C build -j4
+finish build/libdpid.so
--- build-plugins/plugin-mvtoolssf.sh
+++
@@ -1,7 +0,0 @@
-#mkghc IFeelBloated/vapoursynth-mvtools-sf libmvtoolssf 5dfa8756092daa7dcc635eef799c6964bd40c259
-ghdl IFeelBloated/vapoursynth-mvtools-sf
-ghdl IFeelBloated/vsFilterScript
-#ghc IFeelBloated/vsFilterScript dc7efc9a58a388dee45147e1b44bace9118acb36
-CFLAGS="$CFLAGS -Wno-deprecated-declarations" meson build --prefix="$vsprefix"
-ninja -C build -j $JOBS
-ninja -C build install -j $JOBS
