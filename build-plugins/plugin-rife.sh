f7b684d601b2503a316fc284bfd162797f1e7a6f
Georg Pelz <hybrid@selur.de>
2022-10-02 19:23:06 +0200

    switching to styler00dollar/VapourSynth-RIFE-ncnn-Vulkan for RIFE

--- build-plugins/plugin-rife.sh
+++ build-plugins/plugin-rife.sh
@@ -1,3 +1,4 @@
 export CC=/usr/bin/gcc-11
 export CXX=/usr/bin/g++-11
-mkgh HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan librife
+#mkgh HomeOfVapourSynthEvolution/VapourSynth-RIFE-ncnn-Vulkan librife
+mkgh styler00dollar/VapourSynth-RIFE-ncnn-Vulkan librife
