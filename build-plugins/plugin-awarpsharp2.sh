1cd40b3e96f90049c14a5f726d24c7c4b33b1976
protvis74 <50554836+protvis74@users.noreply.github.com>
2024-10-29 19:41:46 +0100

    Update plugin-awarpsharp2.sh

--- build-plugins/plugin-awarpsharp2.sh
+++ build-plugins/plugin-awarpsharp2.sh
@@ -1,1 +1,8 @@
+##################################################################################
+#                                                                                #
+#                               Plugin-awarpsharp2 v4                            #
+#                                                                                #
+#              https://github.com/dubhater/vapoursynth-awarpsharp2               #
+##################################################################################
+
 mkgh dubhater/vapoursynth-awarpsharp2 libawarpsharp2
