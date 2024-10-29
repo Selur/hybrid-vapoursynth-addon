b10838989f47a40ed19b58519f0d83bf930e759f
protvis74 <50554836+protvis74@users.noreply.github.com>
2024-10-29 19:40:46 +0100

    Update plugin-assrender.sh
    Enhanced, extended and improved build system

--- build-plugins/plugin-assrender.sh
+++ build-plugins/plugin-assrender.sh
@@ -1,4 +1,11 @@
-retry_git_clone https://github.com/AmusementClub/assrender
+##################################################################################
+#                                                                                #
+#                          Plugin-AssRender v0.38.2                              #
+#                                                                                #
+#                 https://github.com/AmusementClub/assrender                     #
+##################################################################################
+
+retry_git_clone https://github.com/AmusementClub/assrender assrender
 cd assrender
 cmake -B build -S .
 cmake --build build --clean-first
@@ -9,6 +16,5 @@ strip libassrender.so
 nm -D --extern-only libassrender.so
 mkdir -p "$VSPREFIX/vsplugins"
 cp -f libassrender.so "$VSPREFIX/vsplugins/"
-cd ..
-cd ..
+cd ../../..
 rm -rf assrender
