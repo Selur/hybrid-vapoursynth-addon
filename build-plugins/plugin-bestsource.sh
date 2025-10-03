d9db7be4790b034362a6f8debcceecd79ffb1e16
Georg Pelz (Selur) <hybrid@selur.de>
2025-10-03 16:20:43 +0200

    f3kdb patch not needed any more

--- build-plugins/plugin-bestsource.sh
+++ build-plugins/plugin-bestsource.sh
@@ -8,8 +8,6 @@
 
 git clone https://github.com/vapoursynth/bestsource.git --depth=1 --recurse-submodules --shallow-submodules --remote-submodules build
 cd build
-git fetch --tags
-git checkout R8
 git submodule update --init --recursive --depth=1
 meson setup build -Ddefault_library=static
 ninja -C build
