a2367526742d255a3794d0a47dfed5e2d6b29705
starapo7348 <126355018+starapo7348@users.noreply.github.com>
2026-01-09 14:57:53 +0100

    Update plugin-sangnom.sh / VSPREFIX / meson setup build
    minor corrections
    
    variable $vsprefix -> $VSPREFIX (see config.txt)
    
    meson build -> meson setup build
    
    (WARNING: Running the setup command as meson [options] instead of meson setup [options] is ambiguous and deprecated.)

--- build-plugins/plugin-sangnom.sh
+++ build-plugins/plugin-sangnom.sh
@@ -7,7 +7,6 @@
 ##################################################################################
 
 ghdl dubhater/vapoursynth-sangnom
-CFLAGS="$CFLAGS -Wno-deprecated-declarations" meson build --prefix="$vsprefix"
+CFLAGS="$CFLAGS -Wno-deprecated-declarations" meson setup build --prefix="$VSPREFIX"
 ninja -C build -j $JOBS
 ninja -C build install -j $JOBS
-
