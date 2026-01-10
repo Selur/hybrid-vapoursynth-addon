2522ebc694295591be28f4e2a01ed719aee8dad6
starapo7348 <126355018+starapo7348@users.noreply.github.com>
2026-01-10 16:26:23 +0100

    Update plugin-sangnom.sh
    small fixes. 
    libsangnom.so was not stripped and not copied to /opt/vapoursynth/vsplugins
    Cleanup build-dir.

--- build-plugins/plugin-sangnom.sh
+++ build-plugins/plugin-sangnom.sh
@@ -10,3 +10,7 @@ ghdl dubhater/vapoursynth-sangnom
 CFLAGS="$CFLAGS -Wno-deprecated-declarations" meson setup build --prefix="$VSPREFIX"
 ninja -C build -j $JOBS
 ninja -C build install -j $JOBS
+cd build
+finish libsangnom.so
+cd ../..
+rm -rf build
