999575eeda278ba95b84a091910cc4e8e61e9c1f
starapo7348 <126355018+starapo7348@users.noreply.github.com>
2026-01-10 02:10:33 +0100

    Update plugin-lghost.sh ->revert fix / sudo not needed anymore
    revert one fix ->sudo not needed.
    https://github.com/Selur/hybrid-vapoursynth-addon/pull/35

--- build-plugins/plugin-lghost.sh
+++ build-plugins/plugin-lghost.sh
@@ -10,4 +10,4 @@ ghdl HomeOfVapourSynthEvolution/VapourSynth-LGhost
 
 CFLAGS="$CFLAGS -Wno-deprecated-declarations" meson setup build --prefix="$VSPREFIX"
 ninja -C build -j $JOBS
-sudo ninja -C build install -j $JOBS
+ninja -C build install -j $JOBS
