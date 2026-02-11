181cc843d9f6c1603eaaf0588a6a1a62d3593fbf
Georg Pelz (Selur) <hybrid@selur.de>
2026-02-11 21:21:06 +0100

    Removed sangnommod, see: https://github.com/HomeOfVapourSynthEvolution/VapourSynth-SangNomMod/issues/5

--- build-plugins/plugin-sangnommod.sh
+++
@@ -1,9 +0,0 @@
-##################################################################################
-#                                                                                #
-#                         Plugin-vs-SangNomMod  V0.1-fix                         #
-#                                                                                #
-#                                                                                #
-#    https://github.com/HomeOfVapourSynthEvolution/VapourSynth-SangNomMod        #
-##################################################################################
-
-mkgh HomeOfVapourSynthEvolution/VapourSynth-SangNomMod libsangnommod
